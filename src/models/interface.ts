55778254dd93e9145ba7f42e2e65e91b45234c75
neiyichao03 <nieyichao03@kuaishou.com>
2021-07-04 15:49:23 +0800

    feat: 注释补充

--- src/models/interface.ts
+++ src/models/interface.ts
@@ -1,332 +1,359 @@
+export interface AreaItem {
+	id: string;
+	gridarea: string;
+	areaname: string;
+	customProps?: {
+		// 超市
+		storeGoods?: {
+			// 商品列表
+			goodsList: [
+				// TODO
+			]
+		},
+		suspend?: number; // 停留回纥
+		lucky?: boolean; // 是否抽奖，小羽毛
+	},
+	moneyCost?: number; // 一片地多少钱
+	owner?: undefined | number; // id或者无
+	houseLevel?: 0; // 房子级别
+}
 
- const areaAddressList = [
-    {
-      id:'store',
-      gridarea: "1/1/1/2",
-      areaname: "store 1",
-      customProps: {
-          //超市特有属性
-          storeGoods:{
+export const areaAddressList: Array<AreaItem> = [
+	{
+		id: 'store',
+		gridarea: "1/1/1/2",
+		areaname: "store 1",
+		customProps: {
+			//超市特有属性
+			storeGoods: {
+				goodsList: [],
+			}
+		}
+	},
+	{
+		id: "1",
+		gridarea: "1/2/1/3",
+		areaname: "area1 的第一块地",
+		moneyCost: 1000,
+		owner: undefined,
+		houseLevel: 0,
+	},
+	{
+		id: "1",
+		gridarea: "1/3/1/4",
+		areaname: "area1 的第二块地",
+		moneyCost: 1000,
+		owner: undefined,
+		houseLevel: 0,
+	},
+	{
+		id: "1",
+		gridarea: "1/4/1/5",
+		areaname: "area1 的第三块地",
+		moneyCost: 1000,
+		owner: undefined,
+		houseLevel: 0,
+	},
+	{
+		id: "jail",
+		gridarea: "1/5/1/6",
+		areaname: "jail",
+		customProps: {
+			suspend: 2
+		}
+	},
+	{
+		id: "2",
+		gridarea: "1/6/1/7",
+		areaname: "area2 的第一块地",
+		moneyCost: 1000,
+		owner: undefined,
+		houseLevel: 0,
+	},
+	{
+		id: "2",
+		gridarea: "1/7/1/8",
+		areaname: "area2 的第二块地",
+		moneyCost: 1000,
+		owner: undefined,
+		houseLevel: 0,
+	},
+	{
+		id: "3",
+		gridarea: "1/8/1/9",
+		areaname: "area3 的第一块地",
+		moneyCost: 1000,
+		owner: undefined,
+		houseLevel: 0,
+	},
+	{
+		id: "3",
+		gridarea: "1/9/1/10",
+		areaname: "area3 的第二块地",
+		moneyCost: 1000,
+		owner: undefined,
+		houseLevel: 0,
+	},
+	{
+		id: "3",
+		gridarea: "1/10/1/11",
+		areaname: "area3 的第三块地",
+		moneyCost: 1000,
+		owner: undefined,
+		houseLevel: 0,
+	},
+	{
+		id: "3",
+		gridarea: "1/11/1/12",
+		areaname: "area3 的第四块地",
+		moneyCost: 1000,
+		owner: undefined,
+		houseLevel: 0,
+	},
+	{
+		id: "store",
+		gridarea: "1/12/1/13",
+		areaname: "store 2",
+		customProps: {
+			storeGoods: {
+				goodsList: [],
+			}
+		}
+	},
+	{
+		id: "4",
+		gridarea: "2/12/2/13",
+		areaname: "area4 的第一块地",
+		moneyCost: 1000,
+		owner: undefined,
+		houseLevel: 0,
+	},
+	{
+		id: "4",
+		gridarea: "3/12/3/13",
+		areaname: "area4 的第二块地",
+		moneyCost: 1000,
+		owner: undefined,
+		houseLevel: 0,
+	},
+	{
+		id: "4",
+		gridarea: "4/12/4/13",
+		areaname: "area4 的第三块地",
+		moneyCost: 1000,
+		owner: undefined,
+		houseLevel: 0,
+	},
+	{
+		id: "lucky",
+		gridarea: "5/12/5/13",
+		areaname: "lucky1",
+		customProps: {
+			lucky: true
+		}
+	},
+	{
+		id: "5",
+		gridarea: "6/12/6/13",
+		areaname: "area5 的第一块地",
+		moneyCost: 1000,
+		owner: undefined,
+		houseLevel: 0,
+	},
+	{
+		id: "5",
+		gridarea: "7/12/7/13",
+		areaname: "area5 的第二块地",
+		moneyCost: 1000,
+		owner: undefined,
+		houseLevel: 0,
+	},
+	{
+		id: "5",
+		gridarea: "8/12/8/13",
+		areaname: "area5 的第三块地",
+		moneyCost: 1000,
+		owner: undefined,
+		houseLevel: 0,
+	},
+	{
+		id: "store",
+		gridarea: "9/12/9/13",
+		areaname: "store3",
+		customProps: {
+			storeGoods: {
+				goodsList: [],
+			}
+		}
+	},
+	{
+		id: "6",
+		gridarea: "9/11/9/12",
+		areaname: "area6 的第一块地",
+		moneyCost: 1000,
+		owner: undefined,
+		houseLevel: 0,
+	},
+	{
+		id: "6",
+		gridarea: "9/10/9/11",
+		areaname: "area6 的第二块地",
+		moneyCost: 1000,
+		owner: undefined,
+		houseLevel: 0,
+	},
+	{
+		id: "6",
+		gridarea: "9/9/9/10",
+		areaname: "area6 的第三块地",
+		moneyCost: 1000,
+		owner: undefined,
+		houseLevel: 0,
+	},
+	{
+		id: "hospital",
+		gridarea: "9/8/9/9",
+		areaname: "hospital",
+		customProps: {
+			suspend: 4
+		}
+	},
+	{
+		id: "7",
+		gridarea: "9/7/9/8",
+		areaname: "area7 的第一块地",
+		moneyCost: 1000,
+		owner: undefined,
+		houseLevel: 0,
+	},
+	{
+		id: "7",
+		gridarea: "9/6/9/7",
+		areaname: "area7 的第二块地",
+		moneyCost: 1000,
+		owner: undefined,
+		houseLevel: 0,
+	},
+	{
+		id: "7",
+		gridarea: "9/5/9/6",
+		areaname: "area7 的第三块地",
+		moneyCost: 1000,
+		owner: undefined,
+		houseLevel: 0,
+	},
+	{
+		id: "7",
+		gridarea: "9/4/9/5",
+		areaname: "area7 的第四块地",
+		moneyCost: 1000,
+		owner: undefined,
+		houseLevel: 0,
+	},
+	{
+		id: "lucky",
+		gridarea: "9/3/9/4",
+		areaname: "lucky2",
+		customProps: {
+			lucky: true
+		}
+	},
+	{
+		id: "8",
+		gridarea: "9/2/9/3",
+		areaname: "area8 ",
+		moneyCost: 1000,
+		owner: undefined,
+		houseLevel: 0,
+	},
+	{
+		id: "store",
+		gridarea: "9/1/9/2",
+		areaname: "store3",
+		customProps: {
+			storeGoods: {
+				goodsList: [],
+			}
+		}
+	},
+	{
+		id: "9",
+		gridarea: "8/1/8/2",
+		areaname: "area9 的第四块地",
+		moneyCost: 1000,
+		owner: undefined,
+		houseLevel: 0,
+	},
+	{
+		id: "9",
+		gridarea: "7/1/7/2",
+		areaname: "area9 的第三块地",
+		moneyCost: 1000,
+		owner: undefined,
+		houseLevel: 0,
+	},
+	{
+		id: "9",
+		gridarea: "6/1/6/2",
+		areaname: "area9 的第二块地",
+		moneyCost: 1000,
+		owner: undefined,
+		houseLevel: 0,
+	},
+	{
+		id: "9",
+		gridarea: "5/1/5/2",
+		areaname: "area9 的第一块地",
+		moneyCost: 1000,
+		owner: undefined,
+		houseLevel: 0,
+	},
+	{
+		id: "bank",
+		gridarea: "4/1/4/2",
+		areaname: "bank",
+	},
+	{
+		id: "10",
+		gridarea: "3/1/3/2",
+		areaname: "area10 的第二块地",
+		moneyCost: 1000,
+		owner: undefined,
+		houseLevel: 0,
+	},
+	{
+		id: "10",
+		gridarea: "2/1/2/2",
+		areaname: "area10 的第一块地",
+		moneyCost: 1000,
+		owner: undefined,
+		houseLevel: 0,
+	},
+];
 
-          }
-      }
-    },
-    {
-      id: "1",
-      gridarea: "1/2/1/3",
-      areaname: "area1 的第一块地",
-      moneyCost: 1000,
-      owner: undefined,
-      houseLevel: 0,
-    },
-    {
-      id: "1",
-      gridarea: "1/3/1/4",
-      areaname: "area1 的第二块地",
-      moneyCost: 1000,
-      owner: undefined,
-      houseLevel: 0,
-    },
-    {
-      id: "1",
-      gridarea: "1/4/1/5",
-      areaname: "area1 的第三块地",
-      moneyCost: 1000,
-      owner: undefined,
-      houseLevel: 0,
-    },
-    {
-      id: "jail",
-      gridarea: "1/5/1/6",
-      areaname: "jail",
-      customProps: {
-          suspend : 2
-      }
-    },
-    {
-      id: "2",
-      gridarea: "1/6/1/7",
-      areaname: "area2 的第一块地",
-      moneyCost: 1000,
-      owner: undefined,
-      houseLevel: 0,
-    },
-    {
-      id: "2",
-      gridarea: "1/7/1/8",
-      areaname: "area2 的第二块地",
-      moneyCost: 1000,
-      owner: undefined,
-      houseLevel: 0,
-    },
-    {
-      id: "3",
-      gridarea: "1/8/1/9",
-      areaname: "area3 的第一块地",
-      moneyCost: 1000,
-      owner: undefined,
-      houseLevel: 0,
-    },
-    {
-      id: "3",
-      gridarea: "1/9/1/10",
-      areaname: "area3 的第二块地",
-      moneyCost: 1000,
-      owner: undefined,
-      houseLevel: 0,
-    },
-    {
-      id: "3",
-      gridarea: "1/10/1/11",
-      areaname: "area3 的第三块地",
-      moneyCost: 1000,
-      owner: undefined,
-      houseLevel: 0,
-    },
-    {
-      id: "3",
-      gridarea: "1/11/1/12",
-      areaname: "area3 的第四块地",
-      moneyCost: 1000,
-      owner: undefined,
-      houseLevel: 0,
-    },
-    {
-      id: "store",
-      gridarea: "1/12/1/13",
-      areaname: "store 2",
-      customProps: {
-          storeGoods:{
+export interface UserInfo {
+	userName: string;
+	moneySum: number; // 钱数
+	eachCoin: number; // 每次多少钱
+	image: string; // 头像url
+	stopCount: number; // 停留会和
+}
 
-          }
-      }
-    },
-    {
-      id: "4",
-      gridarea: "2/12/2/13",
-      areaname: "area4 的第一块地",
-      moneyCost: 1000,
-      owner: undefined,
-      houseLevel: 0,
-    },
-    {
-      id: "4",
-      gridarea: "3/12/3/13",
-      areaname: "area4 的第二块地",
-      moneyCost: 1000,
-      owner: undefined,
-      houseLevel: 0,
-    },
-    {
-      id: "4",
-      gridarea: "4/12/4/13",
-      areaname: "area4 的第三块地",
-      moneyCost: 1000,
-      owner: undefined,
-      houseLevel: 0,
-    },
-    {
-      id: "lucky",
-      gridarea: "5/12/5/13",
-      areaname: "lucky1",
-      customProps: {
-          lucky:true
-      }
-    },
-    {
-      id: "5",
-      gridarea: "6/12/6/13",
-      areaname: "area5 的第一块地",
-      moneyCost: 1000,
-      owner: undefined,
-      houseLevel: 0,
-    },
-    {
-      id: "5",
-      gridarea: "7/12/7/13",
-      areaname: "area5 的第二块地",
-      moneyCost: 1000,
-      owner: undefined,
-      houseLevel: 0,
-    },
-    {
-      id: "5",
-      gridarea: "8/12/8/13",
-      areaname: "area5 的第三块地",
-      moneyCost: 1000,
-      owner: undefined,
-      houseLevel: 0,
-    },
-    {
-      id: "store",
-      gridarea: "9/12/9/13",
-      areaname: "store3",
-      customProps: {
-          storeGoods: {
-
-          }
-      }
-    },
-    {
-      id: "6",
-      gridarea: "9/11/9/12",
-      areaname: "area6 的第一块地",
-      moneyCost: 1000,
-      owner: undefined,
-      houseLevel: 0,
-    },
-    {
-      id: "6",
-      gridarea: "9/10/9/11",
-      areaname: "area6 的第二块地",
-      moneyCost: 1000,
-      owner: undefined,
-      houseLevel: 0,
-    },
-    {
-      id: "6",
-      gridarea: "9/9/9/10",
-      areaname: "area6 的第三块地",
-      moneyCost: 1000,
-      owner: undefined,
-      houseLevel: 0,
-    },
-    {
-      id: "hospital",
-      gridarea: "9/8/9/9",
-      areaname: "hospital",
-      customProps: {
-          suspend: 4
-      }
-    },
-    {
-      id: "7",
-      gridarea: "9/7/9/8",
-      areaname: "area7 的第一块地",
-      moneyCost: 1000,
-      owner: undefined,
-      houseLevel: 0,
-    },
-    {
-      id: "7",
-      gridarea: "9/6/9/7",
-      areaname: "area7 的第二块地",
-      moneyCost: 1000,
-      owner: undefined,
-      houseLevel: 0,
-    },
-    {
-      id: "7",
-      gridarea: "9/5/9/6",
-      areaname: "area7 的第三块地",
-      moneyCost: 1000,
-      owner: undefined,
-      houseLevel: 0,
-    },
-    {
-      id: "7",
-      gridarea: "9/4/9/5",
-      areaname: "area7 的第四块地",
-      moneyCost: 1000,
-      owner: undefined,
-      houseLevel: 0,
-    },
-    {
-      id: "lucky",
-      gridarea: "9/3/9/4",
-      areaname: "lucky2",
-      customProps: {
-          lucky:true
-      }
-    },
-    {
-      id: "8",
-      gridarea: "9/2/9/3",
-      areaname: "area8 ",
-      moneyCost: 1000,
-      owner: undefined,
-      houseLevel: 0,
-    },
-    {
-      id: "store",
-      gridarea: "9/1/9/2",
-      areaname: "store3",
-      customProps: {
-          storeGoods: {
-              
-          }
-      }
-    },
-    {
-      id: "9",
-      gridarea: "8/1/8/2",
-      areaname: "area9 的第四块地",
-      moneyCost: 1000,
-      owner: undefined,
-      houseLevel: 0,
-    },
-    {
-      id: "9",
-      gridarea: "7/1/7/2",
-      areaname: "area9 的第三块地",
-      moneyCost: 1000,
-      owner: undefined,
-      houseLevel: 0,
-    },
-    {
-      id: "9",
-      gridarea: "6/1/6/2",
-      areaname: "area9 的第二块地",
-      moneyCost: 1000,
-      owner: undefined,
-      houseLevel: 0,
-    },
-    {
-      id: "9",
-      gridarea: "5/1/5/2",
-      areaname: "area9 的第一块地",
-      moneyCost: 1000,
-      owner: undefined,
-      houseLevel: 0,
-    },
-    {
-      id: "bank",
-      gridarea: "4/1/4/2",
-      areaname: "bank",
-    },
-    {
-      id: "10",
-      gridarea: "3/1/3/2",
-      areaname: "area10 的第二块地",
-      moneyCost: 1000,
-      owner: undefined,
-      houseLevel: 0,
-    },
-    {
-      id: "10",
-      gridarea: "2/1/2/2",
-      areaname: "area10 的第一块地",
-      moneyCost: 1000,
-      owner: undefined,
-      houseLevel: 0,
-    },
-  ];
-
-  const userData = [
-    {
-      username: "小啵",
-      houseCount: 0,
-      moneySum: 10000,
-      eachCoin: 300,
-      image: '',
-      stopCount:0,
-    },
-    {
-      username: "胖啵",
-      houseCount: 0,
-      moneySum: 10000,
-      eachCoin: 300,
-      image: '',
-      stopCount:0
-    },
-  ];
+const userData = [
+	{
+		username: "小啵",
+		houseCount: 0,
+		moneySum: 10000,
+		eachCoin: 300,
+		image: '',
+		stopCount: 0,
+	},
+	{
+		username: "胖啵",
+		houseCount: 0,
+		moneySum: 10000,
+		eachCoin: 300,
+		image: '',
+		stopCount: 0
+	},
+];
